672f66adb4dd53e925812fdd1d970c0abf3d2146
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-08-11 13:50:37 +0530

    🖥️ wip: Outsourced Navlinks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,9 @@
 "use client";
+import { navLinks } from "@/utils";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import React from "react";
 
-const navLinks = [
-  { href: "/", text: "Home" },
-  { href: "/profile", text: "Server Protected Profile" },
-  { href: "/protected", text: "Middleware Protected" },
-  { href: "/auth-protected", text: "Auth Protected" },
-  { href: "/api/data", text: "Protected Api" },
-];
+
 
 const Navbar = () => {
   const { user, error, isLoading } = useUser();
@@ -18,11 +13,19 @@ const Navbar = () => {
       <div className="flex gap-8 justify-center items-center  w-full p-5 border-b border-zinc-600">
         {navLinks &&
           navLinks.map((item, index) => (
-            <a  href={item.href} key={index}>
+            <a href={item.href} key={index}>
               {item.text}
             </a>
           ))}
       </div>
+
+      {user && !isLoading && (
+        <>
+          {/* <SignupButton /> */}
+          {/* <LoginButton /> */}
+        </>
+      )}
+      {user && !isLoading && <>{/* LogoutButton */}</>}
     </div>
   );
 };
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const  navLinks = [
+    { href: "/", text: "Home" },
+    { href: "/profile", text: "Server Protected Profile" },
+    { href: "/protected", text: "Middleware Protected" },
+    { href: "/auth-protected", text: "Auth Protected" },
+    { href: "/api/data", text: "Protected Api" },
+  ];
